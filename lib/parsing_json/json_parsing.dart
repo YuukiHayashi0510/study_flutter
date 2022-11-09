3e9c3bed712bd785edff915f8a55d07ed23e43af
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-11-09 16:01:35 +0900

    feat: json listtile

--- lib/parsing_json/json_parsing.dart
+++ lib/parsing_json/json_parsing.dart
@@ -32,10 +32,11 @@ class _JsonParsingSimpleState extends State<JsonParsingSimple> {
               future: getData(),
               builder: (context, AsyncSnapshot snapshot) {
                 if (snapshot.hasData)
-                  return Text(snapshot.data[0]['title']);
+                  return createListView(snapshot.data, context);
+                // return Text(snapshot.data[0]['title'].toString());
                 else
                   return CircularProgressIndicator();
-              //  loading
+                //  loading
               }),
         ),
       ),
@@ -52,6 +53,34 @@ class _JsonParsingSimpleState extends State<JsonParsingSimple> {
     //   print(value[0]['title'])
     // });
     return data;
+  }
+
+  Widget createListView(List data, BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, int index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Divider(height: 5.0,),
+                ListTile(
+                  title: Text("${data[index]["title"]}"),
+                  subtitle: Text("${data[index]["body"]}"),
+                  leading: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.black26,
+                        radius: 23,
+                        child: Text("${data[index]["id"]}"),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            );
+          }),
+    );
   }
 }
 
